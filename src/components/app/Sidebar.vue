96e6076a5a8d7c7729ec052a834ac9c04a69cd2c
Vitali Semashkevich <vsemashkevich@xbsoftware.by>
2020-11-25 16:22:39 +0300

    initial commit

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+	<ul class="sidenav app-sidenav" :class="{ open: isOpen }">
+		<li v-for="link in links" :key="link.url">
+			<router-link class="waves-effect waves-orange pointer" :to="link.url"
+				>{{ link.title }}
+			</router-link>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		isOpen: { type: Boolean },
+	},
+	data: () => ({
+		links: [
+			{ title: "Счет", url: "/" },
+			{ title: "История", url: "/history" },
+			{ title: "Планирование", url: "/planning" },
+			{ title: "Новая запись", url: "/record" },
+			{ title: "Категории", url: "/categories" },
+		],
+	}),
+};
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="app-main-layout">
+		<Navbar @toggle="toggle" />
+
+		<Sidebar :isOpen="isOpen" />
+
+		<main class="app-content" :class="{ full: !isOpen }">
+			<div class="app-page">
+				<router-view />
+			</div>
+		</main>
+
+		<div class="fixed-action-btn">
+			<a class="btn-floating btn-large blue" href="#">
+				<i class="large material-icons">add</i>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+import Navbar from "@/components/app/Navbar.vue";
+import Sidebar from "@/components/app/Sidebar.vue";
+
+export default {
+	name: "main-layout",
+	components: { Navbar, Sidebar },
+	data: () => ({
+		isOpen: false,
+	}),
+	methods: {
+		toggle() {
+			this.isOpen = !this.isOpen;
+		},
+	},
+};
+</script>
